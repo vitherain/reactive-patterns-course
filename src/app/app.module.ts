8e31555bfb050a878008a22b044f07728b074161
Vít Herain <vit.herain@zonky.cz>
2017-11-12 14:40:40 +0100

    Section 1, Lecture 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserEventExperimentsComponent
   ],
   imports: [
     BrowserModule,
