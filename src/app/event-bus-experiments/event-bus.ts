a7d37d42491f4776dfa5a7a369a0c64ca9337ff5
Vít Herain <vit.herain@zonky.cz>
2017-11-13 20:40:16 +0100

    Section 2, Lecture 15

--- src/app/event-bus-experiments/event-bus.ts
+++ src/app/event-bus-experiments/event-bus.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 export interface Observer {
     notify(data: any);
 }
@@ -15,11 +17,14 @@ class EventBus implements Subject {
     registerObserver(obs: Observer) {
         this.observers.push(obs);
     }
+
     unregisterObserver(obs: Observer) {
-        
+        _.remove(this.observers, el => el === obs);
     }
     notifyObservers(data: any) {
-        
+        this.observers.forEach(obs => obs.notify(data));
     }
     
 }
+
+export const globalEventBus = new EventBus();
